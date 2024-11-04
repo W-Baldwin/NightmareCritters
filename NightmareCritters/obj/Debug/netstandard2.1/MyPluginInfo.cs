5cface84a43d6277fc486ff973beea2ee419a115
Wesley Baldwin <baldwin.48@wright.edu>
2024-11-04 11:56:29 -0500

    Initial commit.

---
+++ NightmareCritters/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace NightmareCritters
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Nuclear_Fox.NightmareCritters";
+        public const string PLUGIN_NAME = "NightmareCritters";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
